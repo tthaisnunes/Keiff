cf49a27180453a810557bf839014a183919d6f96
tthaisnunes <tthaisnsouza@gmail.com>
2018-12-08 00:46:36 -0200

    criação de componente de footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { BannerComponent } from './common/banner/banner.component';
 import { ProductsComponent } from './common/products/products.component';
 import { InstagramPostsComponent } from './common/instagram-posts/instagram-posts.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { InstagramPostsComponent } from './common/instagram-posts/instagram-post
     HomeComponent,
     BannerComponent,
     ProductsComponent,
-    InstagramPostsComponent
+    InstagramPostsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
